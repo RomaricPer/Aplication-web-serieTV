507242c00e2ca6b7d5ab556efaea45c749ef93e0
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-06-10 10:41:56 +0200

    Ajout de la classe tvshowsCollection

---
+++ src/Entity/Collection/tvshowsCollection.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\TvShows.php;
+class tvshowsCollection
+{
+    public static function findAll(): array{
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+        SELECT id, name, overview, posterId
+        FROM tvshows
+        ORDER BY name
+SQL);
+        $stmt -> execute();
+        return $stmt -> fetchAll(\PDO::FETCH_CLASS, TvShows::class);
+    }
+}
