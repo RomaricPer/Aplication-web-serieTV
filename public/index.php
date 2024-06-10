a72c088a34302119e0528e0a0a6a0fa390b14f41
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-06-10 17:27:10 +0200

    AJout lien du fichier CSS + ajout du titre + Modification de classe

--- public/index.php
+++ public/index.php
@@ -6,17 +6,21 @@ use Entity\Collection\TvShowCollection;
 use Entity\Poster;
 
 
-$webPage = new AppWebPage("Séries TV");
+$webPage = new AppWebPage();
 
 $ligne = TvShowCollection::findAll();
+$webPage -> appendCssUrl("css/styles.css");
+$webPage -> setTitle("Séries TV");
 $webPage->appendContent('<div class="container">');
 foreach ($ligne as $tvshow) {
     $webPage->appendContent("<div class='content'>
-                                        <div class='name'>
-                                            <a href=''>{$webPage->escapeString($tvshow->getName())}</a>
-                                        </div>
-                                        <div class='description'>{$webPage->escapeString($tvshow->getOverview())}</div>
                                         <div class='poster'><img src='poster.php?posterId={$tvshow->getPosterId()}'></div>
+                                        <div class='text'>
+                                            <div class='name'>
+                                                <a href=''>{$webPage->escapeString($tvshow->getName())}</a>
+                                            </div>
+                                            <div class='description'>{$webPage->escapeString($tvshow->getOverview())}</div>
+                                        </div>
                              </div>\n");
 }
 $webPage->appendContent('</div>');
