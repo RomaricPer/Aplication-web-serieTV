ff7c21948841a7a0aef3d5e4f838c6db1a67c891
Romaric PERICHARD <romaric.perichard@etudiant.univ-reims.fr>
2024-06-10 15:55:06 +0200

    Ajout de la documentation de la méthode findAll pour le type de retour

--- src/Entity/Collection/TvShowCollection.php
+++ src/Entity/Collection/TvShowCollection.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 namespace Entity\Collection;
 
 use Database\MyPdo;
-use Entity\TvShows;
-class tvshowsCollection
+use Entity\TvShow;
+class TvShowCollection
 {
+    /**
+     * @return TvShow[]
+     */
     public static function findAll(): array{
         $stmt = MyPdo::getInstance()->prepare(
             <<<'SQL'
         SELECT id, name, overview, posterId
-        FROM tvshows
+        FROM tvshow
         ORDER BY name
 SQL);
         $stmt -> execute();
-        return $stmt -> fetchAll(\PDO::FETCH_CLASS, TvShows::class);
+        return $stmt -> fetchAll(\PDO::FETCH_CLASS, TvShow::class);
     }
 }
