3a3fbaeba67a3f68efd7f4584ba55cb2845ae2cd
Romaric PERICHARD <romaric.perichard@etudiant.univ-reims.fr>
2024-06-10 16:17:59 +0200

    Ajout d'un message d'erreur dans le parameterException

--- public/poster.php
+++ public/poster.php
@@ -9,11 +9,10 @@ try {
     if (isset($_GET['posterId']) && (ctype_digit($_GET['posterId']))) {
         $poster = \Entity\Poster::findById((int)$_GET['posterId']);
     } else {
-        throw new ParameterException();
+        throw new ParameterException("id de poster non valide");
     }
     header('Content-Type: image/jpeg');
     echo $poster->getJpeg();
-
 } catch (ParameterException) {
     http_response_code(400);
 } catch (EntityNotFoundException) {
