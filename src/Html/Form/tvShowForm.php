849787749b9adf14fad862910438df1f43517e69
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2024-06-11 16:19:27 +0200

    Ajout de la méthode getHtmlForm

--- src/Html/Form/tvShowForm.php
+++ src/Html/Form/tvShowForm.php
@@ -18,5 +18,39 @@ class tvShowForm{
         {
             return $this->tvShow;
         }
+    public function getHtmlForm(string $action): string
+    {
+        return <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="utf8">
+            <title></title>
+        </head>
+        <body> 
+            <form method="post" action="{$action}">
+            <input name="id" type="hidden" value="{$this->tvShow?->getId()}">
+                <label> 
+                    Name
+                    <input name="name" type="text" value="{$this->escapeString($this->tvShow?->getName())}" required>
+                </label>
+                <label>
+                    Original Name
+                    <input name="original_name" type="text" value="{$this->escapeString($this->tvShow?->getOriginalName())}" required>
+                </label>
+                <label>
+                    Home page
+                    <input name="homepage" type="text" value="{$this->escapeString($this->tvShow?->getHomepage())}" required>
+                </label>
+                <label>
+                    Overview
+                    <input name="overview" type="text" value="{$this->escapeString($this->tvShow?->getOverview())}" required>
+                </label>
+                <button type="submit">Save</button>
+            </form>
+        </body>
+        </html>
+    HTML;
+    }
     }
 
