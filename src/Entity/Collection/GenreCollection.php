2f0370261843b767e501dd0a9a9943a22bbda941
Romaric PERICHARD <romaric.perichard@etudiant.univ-reims.fr>
2024-06-12 15:40:08 +0200

    Création de la classe GenreCollection qui possède la méthode findAll()

---
+++ src/Entity/Collection/GenreCollection.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Genre;
+class GenreCollection
+{
+    public static function findAll():array
+    {
+        $stmt = MyPdo::getInstance()->prepare(<<<'SQL'
+        SELECT id, name
+        FROM genre
+        ORDER BY name
+        SQL);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, Genre::class);
+    }
+}
