88e732f491a9d9b7ea12d0da8a55c1f847b9023d
Romaric PERICHARD <romaric.perichard@etudiant.univ-reims.fr>
2024-06-12 16:32:29 +0200

    Ajout de la méthode findByGenreId dans TvShowCollection.php

--- src/Entity/Collection/TvShowCollection.php
+++ src/Entity/Collection/TvShowCollection.php
@@ -20,4 +20,17 @@ SQL);
         $stmt -> execute();
         return $stmt -> fetchAll(\PDO::FETCH_CLASS, TvShow::class);
     }
+    public static function findByGenreId(int $genreId):array
+    {
+        $stmt = MyPdo::getInstance()->prepare(<<<'SQL'
+        SELECT id,name,overview,posterId
+        FROM tvshow
+        WHERE id IN (SELECT tvShowId
+                     FROM tvshow_genre
+                     WHERE genreId = :genreId)
+        ORDER BY name
+        SQL);
+        $stmt->execute(['genreId'=>$genreId]);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS,TvShow::class);
+    }
 }
