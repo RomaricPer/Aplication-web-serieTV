ec4d17a1319baeb1107f0d8d2b0600b847ed5d5a
Romaric PERICHARD <romaric.perichard@etudiant.univ-reims.fr>
2024-06-12 17:00:33 +0200

    Ajout d'un formulaire qui filtre les séries

--- public/index.php
+++ public/index.php
@@ -1,17 +1,42 @@
 <?php
 declare(strict_types=1);
 
+use Entity\Collection\GenreCollection;
 use Html\AppWebPage;
 use Entity\Collection\TvShowCollection;
-use Entity\Poster;
-
 
 $webPage = new AppWebPage();
 
-$ligne = TvShowCollection::findAll();
+if (isset($_GET['genre']) && ctype_digit($_GET['genre'])) {
+    $genreId = (int)$_GET['genre'];
+    $ligne = TvShowCollection::findByGenreId($genreId);
+} else {
+    $ligne = TvShowCollection::findAll();
+}
+
+$ligneGenre = GenreCollection::findAll();
 $webPage -> appendCssUrl("css/styles.css");
 $webPage -> setTitle("Séries TV");
 $webPage -> appendButton("create","Créer", "tvshow-form.php");
+$webPage->appendContent(<<<HTML
+                        <form action="index.php">
+                            <label>
+                                Sélectionner un genre
+                            </label>
+                            <select name="genre">
+HTML);
+
+foreach ($ligneGenre as $genre) {
+    $webPage->appendContent(<<<HTML
+                        <option value="{$genre->getId()}">{$genre->getName()}</option>
+HTML);}
+
+$webPage->appendContent(<<<HTML
+        </select>
+     <button type="submit">Enregistrer</button>
+</form>
+HTML
+);
 $webPage->appendContent('<div class="container">');
 foreach ($ligne as $tvshow) {
     $webPage->appendContent("<a href='tvshow.php?tvShowId={$tvshow->getId()}'>
